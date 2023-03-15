e69efd977cd70b9f213afcd4f1a39a2c60f28ce5
apichaya.k <apichaya.k@clicknext.com>
2023-03-15 10:33:05 +0700

    fix code

--- app.vue
+++ app.vue
@@ -2,9 +2,38 @@
   <div>
     <NuxtLink
       to="/"
-      class="font-semibold text-gray-50 hover:text-gray-400 duration-100"
+      class="font-semibold text-gray-500 hover:text-gray-400 duration-100"
     >
       Home
     </NuxtLink>
+    <img src="./assets/img/ic_smartplay.svg" />
+    <div>
+      <svg
+        width="132"
+        height="33"
+        viewBox="0 0 132 33"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M0 4C0 1.79086 1.79086 0 4 0H106.23C107.518 0 108.727 0.619871 109.478 1.66546L130.862 31.4164C131.337 32.0779 130.864 33 130.05 33H107.25H4C1.79086 33 0 31.2091 0 29V4Z"
+          fill="url(#paint0_linear)"
+        />
+        <defs>
+          <linearGradient
+            id="paint0_linear"
+            x1="-5.90635"
+            y1="0"
+            x2="-5.90635"
+            y2="35.9532"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset="0" stop-color="#9A7DB9" />
+            <stop offset="0.497809" stop-color="#755E9B" />
+            <stop offset="1" stop-color="#574C76" />
+          </linearGradient>
+        </defs>
+      </svg>
+    </div>
   </div>
 </template>
